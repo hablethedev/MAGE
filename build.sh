b7f669c650aafdbe3f4dc3dfa84038b8e7147acb
Omri <omri.hermon@gmail.com>
2024-12-31 15:41:50 +0200

    more stuff that does stuff that fixes stuff to do stuff

--- build.sh
+++ build.sh
@@ -1,2 +1,1 @@
-# g++ -o bin/mage src/main.cpp src/glad.c -lglfw -lGL -ldl -Isrc/include/
 g++ -o bin/mage src/main.cpp src/glad.c src/imgui/imgui.cpp src/imgui/imgui_draw.cpp src/imgui/imgui_widgets.cpp src/imgui/imgui_tables.cpp src/imgui/imgui_impl_glfw.cpp src/imgui/imgui_impl_opengl3.cpp -I src/include -lglfw -lGL -ldl
