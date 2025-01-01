7a7b05c11cb01bc743c998a0cd8770489a39237e
Omri <omri.hermon@gmail.com>
2025-01-01 12:42:51 +0200

    aaa

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <glad/glad.h>
 #include "imgui/imgui.h"
-#include "imgui/imgui_impl_glfw.h"
-#include "imgui/imgui_impl_opengl3.h"
+#include "imgui/backends/imgui_impl_glfw.h"
+#include "imgui/backends/imgui_impl_opengl3.h"
 #include <GLFW/glfw3.h>
 #include <iostream>
 #include <chrono>
